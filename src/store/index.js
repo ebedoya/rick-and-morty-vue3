639474637618f77387075de3c44a36825f337f90
Edwin R <edwin0923@gmail.com>
2021-08-13 18:43:15 -0500

    Project rick and morty in vue 3

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,47 @@
-import { createStore } from 'vuex'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-export default createStore({
+export default new Vuex.Store({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload
+    },
+    setCharactersFilter(state, payload) {
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters({ commit }) {
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        commit('setCharacters', data.results)
+        commit('setCharactersFilter', data.results)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    filterByStatus({ commit, state }, status) {
+      const results = state.characters.filter((character) => {
+        return character.status.includes(status)
+      })
+      commit('setCharactersFilter', results)
+    },
+    filterByName({ commit, state }, name) {
+      const formatName = name.toLowerCase()
+      const results = state.characters.filter((character) => {
+        const characterName = character.name.toLowerCase()
+
+        if(characterName.includes(formatName)) {
+          return character
+        }
+      })
+      commit('setCharactersFilter', results)
+    }
   },
   modules: {
   }
